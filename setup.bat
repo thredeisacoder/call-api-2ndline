8b0c1cfe6a25ad82935966feaf743b847eb0629a
Threde <67854175+thredeisacoder@users.noreply.github.com>
2025-01-10 13:58:53 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -7,7 +7,7 @@ echo =========================================
 node -v >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (
     echo [!] Node.js is not installed. Downloading Node.js installer...
-    powershell -Command "Start-Process 'https://nodejs.org/dist/v20.12.1/node-v20.12.1-x64.msi' -Wait"
+    powershell -Command "Start-Process 'https://nodejs.org/dist/v22.13.0/node-v22.13.0-x64.msi' -Wait"
     echo [!] Please install Node.js and re-run this script.
     pause
     exit /b
