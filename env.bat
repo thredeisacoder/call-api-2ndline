5f65405019c1101e1af7af75c541d8ad0a7c4b07
Threde <67854175+thredeisacoder@users.noreply.github.com>
2025-01-10 13:24:41 +0700

    Add files via upload

---
+++ env.bat
@@ -0,0 +1,10 @@
+@echo off
+echo =========================================
+echo   Environment  - Automatic Setup
+echo =========================================
+
+:: Khởi tạo npm và cài đặt thư viện
+echo [*] Setting up the project...
+npm init -y
+npm install express ejs body-parser axios
+
